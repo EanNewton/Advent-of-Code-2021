8440e9473791afa902ab9cef38619d4b581de031
EanNewton <eannewton5@gmail.com>
2021-12-09 13:49:38 -0700

    day 1

--- main.py
+++ main.py
@@ -20,15 +20,15 @@ def day1_p1(raw: list) -> int:
     return len([x for x, y in zip(raw, raw[1:]) if x < y])
 
 
-def day1_p2(raw: list) -> list:
+def day1_p2(raw: list) -> int:
     """
     Your goal now is to count the number of times the sum of
     measurements in this sliding window increases from the previous sum.
     :param raw: list
     :return: int
     """
-    return [sum(raw[idx:idx + 3]) for idx in range(len(raw)) if idx < len(raw) - 2]
+    return len([x for x, y in zip([sum(raw[idx:idx + 3]) for idx in range(len(raw)) if idx < len(raw) - 2], [sum(raw[idx:idx + 3]) for idx in range(len(raw)) if idx < len(raw) - 2][1:]) if x < y])
 
 
 if __name__ == '__main__':
-    print(day2(map_to_int_list(multi_in())))
+    print(day1_p2(map_to_int_list(multi_in())))
