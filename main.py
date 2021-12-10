6d9cbd4b408eaa3a8529173e680f4ad3e63f2aab
EanNewton <eannewton5@gmail.com>
2021-12-09 23:20:28 -0700

    cleaned

--- main.py
+++ main.py
@@ -10,26 +10,20 @@ __version__ = "0.1.0"
 __license__ = "AGPL-3.0"
 
 import sys
-
-f_='forward'
-u_='up'
-d_='down'
-rl_=lambda _:range(len(_))
-r_=lambda _:range(_)
-
+f_,u_,d_='forward','up','down'
+i_,l_,r_=int,len,range
+rl_=lambda _:r_(l_(_))
 def d11(r):
-    return len([x for x,y in zip(r,r[1:])if x<y])
+ return l_([x for x,y in zip(r,r[1:])if x<y])
 def d12(r):
-    l=len(r)
-    return len([x for x,y in zip([sum(r[i:i+3])for i in r_(l)if i<l-2],[sum(r[i:i+3])for i in r_(l)if i<l-2][1:])if x<y])
+ return l_([x for x,y in zip([sum(r[i:i+3])for i in rl_(r)if i<l_(r)-2],[sum(r[i:i+3])for i in rl_(r)if i<l_(r)-2][1:])if x<y])
 def d21(r,m=[[0,0]]):
-    for o,v in[(o,int(v))for _ in r for(o,v)in[_.split()]]:m=[[m[i][j]+{f_:[[v,0]],u_:[[0,v*-1]],d_:[[0,v]]}.get(o,None)[i][j]for j in rl_(m[0])]for i in rl_(m)]
-    return m[0][0]*m[0][1]
+ for o,v in[(o,i_(v))for _ in r for(o,v)in[_.split()]]:m=[[m[i][j]+{f_:[[v,0]],u_:[[0,v*-1]],d_:[[0,v]]}.get(o,None)[i][j]for j in rl_(m[0])]for i in rl_(m)]
+ return m[0][0]*m[0][1]
 def d22(a,b=[[0,0,0]]):
-    for d,e in[(d,int(e))for _ in a for(d,e)in[_.split()]]:b=[[b[i][j]+{f_:[[e,e*b[0][2],0]],u_:[[0,0,e*-1]],d_:[[0,0,e]]}.get(d,None)[i][j]for j in rl_(b[0])]for i in rl_(b)]
-    return b[0][0]*b[0][1]
+ for d,e in[(d,i_(e))for _ in a for(d,e)in[_.split()]]:b=[[b[i][j]+{f_:[[e,e*b[0][2],0]],u_:[[0,0,e*-1]],d_:[[0,0,e]]}.get(d,None)[i][j]for j in rl_(b[0])]for i in rl_(b)]
+ return b[0][0]*b[0][1]
 def d31(r):
-    return(lambda _:_[0]*_[1])([int(''.join([f(set(e),key=e.count)for e in[([_[e]for _ in r])for e in rl_(r[0])]]),2)for f in(min,max)])
-
+ return(lambda _:_[0]*_[1])([i_(''.join([f(set(e),key=e.count)for e in[([_[e]for _ in r])for e in rl_(r[0])]]),2)for f in(min,max)])
 if __name__ == '__main__':
-    print(globals()['d'+input('>')](sys.stdin.readlines())) # Ctrl-D to EOF
+ print(globals()['d'+input('>')](sys.stdin.readlines())) # Ctrl-D to EOF
