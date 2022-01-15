d435b57bd9df40448bfd6be2d0d55955966cffd1
EanNewton <eannewton5@gmail.com>
2022-01-14 17:25:07 -0700

    Day 5 part 1 Naive

--- day5.py
+++ day5.py
@@ -1,6 +1,6 @@
 from cutil import multi_in
 
-def filter_input(raw: str) -> list:
+def filter_input(raw: str) -> tuple:
     lines = [each.split(" -> ") for each in raw.splitlines()]
     coords = []
     max_ = 0
@@ -26,7 +26,6 @@ def write_board(board: list) -> None:
             row = [str(_) if _ != 0 else "." for _ in row]
             row = ' '.join(row) + "\n"
             f.write(row)
-    return None
 
 
 def score_board(board: list) -> int:
@@ -47,14 +46,20 @@ def d51(raw: str) -> int:
     coords, max_ = filter_input(raw)
     board = [[0 for _ in range(max_)] for i in range(max_)]
     for each in coords:
+        # Vertical lines
         if each["x1"] == each["x2"]:
             if each["y1"] > each["y2"]:
+                # We can only draw from top to bottom in naive solution
+                # If the coords indicate the opposite we need to flip
                 each["y1"], each["y2"] = each["y2"], each["y1"]
                 each["x1"], each["x2"] = each["x2"], each["x1"]
             for pos in range(each["y1"], each["y2"]+1):
                 board[pos][each["x1"]] += 1
+        # Horizontal lines
         elif each["y1"] == each["y2"]:
             if each["x1"] > each["x2"]:
+                # We can only draw from left to right in naive solution
+                # If the coords indicate the opposite we need to flip
                 each["y1"], each["y2"] = each["y2"], each["y1"]
                 each["x1"], each["x2"] = each["x2"], each["x1"]
             for pos in range(each["x1"], each["x2"]+1):
@@ -62,6 +67,60 @@ def d51(raw: str) -> int:
     #print_board(board)
     return score_board(board)
 
+
+def d52(raw: str) -> int:
+    """
+    At how many points do at least two lines overlap?
+    :param raw: user input
+    :return: int
+    """
+    coords, max_ = filter_input(raw)
+    board = [[0 for _ in range(max_)] for i in range(max_)]
+    for each in coords:
+        # Vertical lines
+        if each["x1"] == each["x2"]:
+            if each["y1"] > each["y2"]:
+                # We can only draw from top to bottom in naive solution
+                # If the coords indicate the opposite we need to flip
+                each["y1"], each["y2"] = each["y2"], each["y1"]
+                each["x1"], each["x2"] = each["x2"], each["x1"]
+            for pos in range(each["y1"], each["y2"]+1):
+                board[pos][each["x1"]] += 1
+        # Horizontal lines
+        elif each["y1"] == each["y2"]:
+            if each["x1"] > each["x2"]:
+                # We can only draw from left to right in naive solution
+                # If the coords indicate the opposite we need to flip
+                each["y1"], each["y2"] = each["y2"], each["y1"]
+                each["x1"], each["x2"] = each["x2"], each["x1"]
+            for pos in range(each["x1"], each["x2"]+1):
+                board[each["y1"]][pos] += 1
+        # Diagonal Lines
+        elif abs(each["x1"] - each["x2"]) == abs(each["y1"] - each["y2"]):
+            print("diag found at: {},{} {},{}".format(
+                each["x1"],
+                each["y1"],
+                each["x2"],
+                each["y2"]
+            ))
+
+            # if each["y1"] > each["y2"]:
+            #     # We can only draw from top to bottom in naive solution
+            #     # If the coords indicate the opposite we need to flip
+            #     each["y1"], each["y2"] = each["y2"], each["y1"]
+            #     #each["x1"], each["x2"] = each["x2"], each["x1"]
+            # if each["x1"] > each["x2"]:
+            #     # We can only draw from left to right in naive solution
+            #     # If the coords indicate the opposite we need to flip
+            #     #each["y1"], each["y2"] = each["y2"], each["y1"]
+            #     each["x1"], each["x2"] = each["x2"], each["x1"]
+            xs = range(each["x1"], each["x2"]+1)
+            ys = range(each["y1"], each["y2"]+1)
+            print(xs, ys)
+            for x, y in [xs, ys]:
+                board[x][y] += 1
+    print_board(board)
+    return score_board(board)
 
 if __name__ == '__main__':
     # print(globals()['d' + input('>')](multi_in()))  # ENTER to EOF
